c129b8831bea6ce984a20d546334a1b9b9a28317
mwidjaja24 <43392840+mwidjaja24@users.noreply.github.com>
2020-10-18 06:30:43 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,16 +8,16 @@ This will be updated by using a db instead of a hard coded array
 */
 app.get("/api/getTitle", (req,res) => {
     const titles = [
-        {id:1, title: "Attack On Titan"},
-        {id:2, title: "Sword Art Online"},
-        {id:3, title: "Naruto"},
-        {id:4, title: "Tokyo Ghoul"},
-        {id:5, title: "Re:Zero"},
-        {id:6, title: "One Piece"},
-        {id:7, title: "My Hero Academia"},
-        {id:8, title: "Demon Slayer"},
-        {id:9, title: "Konosuba"},
-        {id:10, title: "Black Clover"}
+        {id:1, title: "Attack On Titan",url:"https://cdn.myanimelist.net/images/anime/10/47347l.jpg"},
+        {id:2, title: "Sword Art Online",url:"https://cdn.myanimelist.net/images/anime/11/39717.jpg"},
+        {id:3, title: "Naruto",url:"https://cdn.myanimelist.net/images/anime/13/17405.jpg"},
+        {id:4, title: "Tokyo Ghoul",url:"https://cdn.myanimelist.net/images/anime/5/64449.jpg"},
+        {id:5, title: "Re:Zero",url:"https://cdn.myanimelist.net/images/anime/11/79410.jpg"},
+        {id:6, title: "One Piece",url:"https://cdn.myanimelist.net/images/anime/6/73245.jpg"},
+        {id:7, title: "My Hero Academia",url:"https://cdn.myanimelist.net/images/anime/10/78745.jpg"},
+        {id:8, title: "Demon Slayer",url:"https://cdn.myanimelist.net/images/anime/1286/99889.jpg"},
+        {id:9, title: "Konosuba",url:"https://cdn.myanimelist.net/images/anime/8/77831.jpg"},
+        {id:10, title: "Black Clover",url:"https://cdn.myanimelist.net/images/anime/2/88336.jpg"}
     ]
 
     res.json(titles);
