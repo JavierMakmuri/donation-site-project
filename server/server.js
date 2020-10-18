4689e0c2f90f2ec7a0de43095be843480030f9de
Javier <javiermakmuri@yahoo.com>
2020-10-18 19:13:11 +0700

    add 10 titles in server.js

--- server/server.js
+++ server/server.js
@@ -8,9 +8,16 @@ This will be updated by using a db instead of a hard coded array
 */
 app.get("/api/getTitle", (req,res) => {
     const titles = [
-        {id:1, title: "John"},
-        {id:2, title: "Bob"},
-        {id:3, title: "Mary"}
+        {id:1, title: "Attack On Titan"},
+        {id:2, title: "Sword Art Online"},
+        {id:3, title: "Naruto"},
+        {id:4, title: "Tokyo Ghoul"},
+        {id:5, title: "Re:Zero"},
+        {id:6, title: "One Piece"},
+        {id:7, title: "My Hero Academia"},
+        {id:8, title: "Demon Slayer"},
+        {id:9, title: "Konosuba"},
+        {id:10, title: "Black Clover"}
     ]
 
     res.json(titles);
