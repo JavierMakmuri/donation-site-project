678d9f1ee52d356a34ed1b80bbfe653a4f91c543
Raymond <anggonoraymond@gmail.com>
2020-11-13 23:14:38 +0700

    Add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Titles from './components/titles.js';
+import React from "react";
+import Titles from "./components/titles.js";
+import Navbar from "./components/navbar.js";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
       <Titles />
     </div>
   );
