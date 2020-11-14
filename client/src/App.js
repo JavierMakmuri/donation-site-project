74dfc794cfb25563b1a6c47f413aadc3bd23f064
Javier Makmuri <javiermakmuri26@gmail.com>
2020-11-14 15:39:00 +0700

    fix display error

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Titles from "./components/titles.js";
 import Navbar from "./components/navbar.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Titles />
     </div>
   );
 }
