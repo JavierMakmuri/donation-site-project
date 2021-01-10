293e3015a36b46178e59715fc35d84393474cbb1
Javier M <javiermakmuri26@gmail.com>
2021-01-10 14:33:32 +0000

    add more titles

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const database = [
   {title: "Attack On Titan", url:"https://cdn.myanimelist.net/images/anime/10/47347l.jpg"},
   {title: "Sword Art Online", url:"https://cdn.myanimelist.net/images/anime/11/39717.jpg"},
-  {title: "Naruto", url:"https://cdn.myanimelist.net/images/anime/13/17405.jpg"},
+  {title: "Naruto", url:"https://cdn.myanimelist.net/images/anime/5/17407.jpg"},
   {title: "Tokyo Ghoul", url:"https://cdn.myanimelist.net/images/anime/5/64449.jpg"},
   {title: "Re:Zero", url:"https://cdn.myanimelist.net/images/anime/11/79410.jpg"},
   {title: "One Piece", url:"https://cdn.myanimelist.net/images/anime/6/73245.jpg"},
@@ -10,7 +10,22 @@ const database = [
   {title: "Demon Slayer", url:"https://cdn.myanimelist.net/images/anime/1286/99889.jpg"},
   {title: "Konosuba" ,url:"https://cdn.myanimelist.net/images/anime/8/77831.jpg"},
   {title: "Black Clover", url:"https://cdn.myanimelist.net/images/anime/2/88336.jpg"},
-  {title: "Hunter x Hunter", url:"https://cdn.myanimelist.net/images/anime/11/33657.jpg"}
+  {title: "Hunter x Hunter", url:"https://cdn.myanimelist.net/images/anime/11/33657.jpg"},
+  {title: "Dr. Stone", url:"https://cdn.myanimelist.net/images/anime/1711/110614.jpg"},
+  {title: "The Promised Neverland", url:"https://cdn.myanimelist.net/images/anime/1125/96929.jpg"},
+  {title: "Horimiya", url:"https://cdn.myanimelist.net/images/anime/1791/110336.jpg"},
+  {title: "Violet Evergarden", url:"https://cdn.myanimelist.net/images/anime/1795/95088.jpg"},
+  {title: "Haikyu!!", url: "https://cdn.myanimelist.net/images/anime/7/76014.jpg"},
+  {title: "One Punch Man", url: "https://cdn.myanimelist.net/images/anime/12/76049.jpg"},
+  {title: "The Seven Deadly Sins", url: "https://cdn.myanimelist.net/images/anime/8/65409.jpg"},
+  {title: "Fullmetal Alchemist", url: "https://cdn.myanimelist.net/images/anime/1223/96541.jpg"},
+  {title: "Gintama", url: "https://cdn.myanimelist.net/images/anime/3/72078.jpg"},
+  {title: "Your Lie in April", url: "https://cdn.myanimelist.net/images/anime/3/67177.jpg"},
+  {title: "Vinland Saga", url: "https://cdn.myanimelist.net/images/anime/1500/103005.jpg"},
+  {title: "JoJo's Bizarre Adventure", url: "https://cdn.myanimelist.net/images/anime/3/40409.jpg"},
+  {title: "Steins;Gate", url: "https://cdn.myanimelist.net/images/anime/5/73199.jpg"},
+  {title: "My youth romantic comedy is wrong as I expected", url: "https://cdn.myanimelist.net/images/anime/1958/107912.jpg"},
+  {title: "Rascal Does Not Dream of Bunny Girl Senpai", url: "https://cdn.myanimelist.net/images/anime/1301/93586.jpg"}
 ]
 
 const express = require("express");
